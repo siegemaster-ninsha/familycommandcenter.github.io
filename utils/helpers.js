89f67c859b22981a93d7aca3c48504251bd98583
siegemaster-ninsha <ninsha@gmail.com>
2025-10-05 09:36:26 -0400

    update

--- utils/helpers.js
+++ utils/helpers.js
@@ -73,7 +73,6 @@
       eyeOff: () => `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>`
     },
 
-
     // Helper function to get icon by name and library
     getIcon(iconName, library = 'lucide', size = 16, className = '') {
       // Handle case where getIcon is called before Helpers is fully initialized
@@ -99,49 +98,43 @@
       if (typeof lucide !== 'undefined') {
         lucide.createIcons();
       }
+    },
+
+    // Helper functions for UI functionality
+    getCategoryIcon(category) {
+      switch (category) {
+        case 'school':
+          return `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+              <path d="M208,24H72A32,32,0,0,0,40,56V224a8,8,0,0,0,8,8H192a8,8,0,0,0,0-16H56a16,16,0,0,1,16-16H208a8,8,0,0,0,8-8V32A8,8,0,0,0,208,24ZM72,40H200V184H72a31.82,31.82,0,0,0-16,4.29V56A16,16,0,0,1,72,40Z"></path>
+            </svg>`;
+        case 'game':
+          return `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+              <path d="M192,88h16a8,8,0,0,1,0,16H192a8,8,0,0,1,0-16ZM48,104H64a8,8,0,0,0,0-16H48a8,8,0,0,0,0,16ZM208,40H48A24,24,0,0,0,24,64V192a24,24,0,0,0,24,24H208a24,24,0,0,0,24-24V64A24,24,0,0,0,208,40ZM216,192a8,8,0,0,1-8,8H48a8,8,0,0,1-8-8V64a8,8,0,0,1,8-8H208a8,8,0,0,1,8,8V192Z"></path>
+            </svg>`;
+        default:
+          return `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+              <path d="M218.83,103.77l-80-75.48a1.14,1.14,0,0,1-.11-.11,16,16,0,0,0-21.53,0l-.11.11L37.17,103.77A8,8,0,0,0,32,110.62V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V110.62A8,8,0,0,0,218.83,103.77ZM208,208H48V115.54L128,44.77,208,115.54V208ZM112,176V136a8,8,0,0,1,8-8h16a8,8,0,0,1,8,8v40a8,8,0,0,1-16,0V144H120v32a8,8,0,0,1-16,0Z"></path>
+            </svg>`;
+      }
+    },
+
+    getCategoryLabel(category) {
+      switch (category) {
+        case 'school': return 'School';
+        case 'game': return 'Electronics';
+        default: return 'Regular';
+      }
+    },
+
+    isChoreSelected(selectedChoreId, selectedQuicklistChore, chore) {
+      if (!chore) return false;
+      if (selectedQuicklistChore && selectedQuicklistChore.name === chore.name) return true;
+      return !!(selectedChoreId && chore.id === selectedChoreId);
     }
   };
 
   // Attach IconLibrary to window.Helpers immediately
   window.Helpers.IconLibrary = IconLibrary;
-
-  function getCategoryIcon(category) {
-    switch (category) {
-      case 'school':
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-            <path d="M208,24H72A32,32,0,0,0,40,56V224a8,8,0,0,0,8,8H192a8,8,0,0,0,0-16H56a16,16,0,0,1,16-16H208a8,8,0,0,0,8-8V32A8,8,0,0,0,208,24ZM72,40H200V184H72a31.82,31.82,0,0,0-16,4.29V56A16,16,0,0,1,72,40Z"></path>
-          </svg>`;
-      case 'game':
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-            <path d="M192,88h16a8,8,0,0,1,0,16H192a8,8,0,0,1,0-16ZM48,104H64a8,8,0,0,0,0-16H48a8,8,0,0,0,0,16ZM208,40H48A24,24,0,0,0,24,64V192a24,24,0,0,0,24,24H208a24,24,0,0,0,24-24V64A24,24,0,0,0,208,40ZM216,192a8,8,0,0,1-8,8H48a8,8,0,0,1-8-8V64a8,8,0,0,1,8-8H208a8,8,0,0,1,8,8V192Z"></path>
-          </svg>`;
-      default:
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-            <path d="M218.83,103.77l-80-75.48a1.14,1.14,0,0,1-.11-.11,16,16,0,0,0-21.53,0l-.11.11L37.17,103.77A8,8,0,0,0,32,110.62V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V110.62A8,8,0,0,0,218.83,103.77ZM208,208H48V115.54L128,44.77,208,115.54V208ZM112,176V136a8,8,0,0,1,8-8h16a8,8,0,0,1,8,8v40a8,8,0,0,1-16,0V144H120v32a8,8,0,0,1-16,0Z"></path>
-          </svg>`;
-    }
-  }
-
-  function getCategoryLabel(category) {
-    switch (category) {
-      case 'school': return 'School';
-      case 'game': return 'Electronics';
-      default: return 'Regular';
-    }
-  }
-
-  function isChoreSelected(selectedChoreId, selectedQuicklistChore, chore) {
-    if (!chore) return false;
-    if (selectedQuicklistChore && selectedQuicklistChore.name === chore.name) return true;
-    return !!(selectedChoreId && chore.id === selectedChoreId);
-
-  // Update window.Helpers with all the helper functions
-  window.Helpers = {
-    ...window.Helpers,
-    getCategoryIcon,
-    getCategoryLabel,
-    isChoreSelected
-  };
 })();
 
 
