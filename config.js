28244dd2b997260e4af37b7b5d91f3f6054d4ef0
siegemaster-ninsha <ninsha@gmail.com>
2025-12-14 13:08:29 -0500

    Fix: Add v-cloak to prevent Vue template flash before mount

--- config.js
+++ config.js
@@ -64,7 +64,7 @@ const CONFIG = {
   // Application Settings
   APP: {
     NAME: 'Family Command Center',
-    VERSION: '1.0.6 - Eager Impala (Dec 14, 2025)',
+    VERSION: '1.0.7 - Nimble Raccoon (Dec 14, 2025)',
     
     // Chore Categories (safe to be public)
     CATEGORIES: {
