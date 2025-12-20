5d8d7fb109599912f357b430add28f4614928ed1
siegemaster-ninsha <ninsha@gmail.com>
2025-12-20 10:22:22 -0500

    fix: calendar widget off-by-one day issue for all-day events

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.83-1766242859';
+const CACHE_VERSION = 'v1.0.84-1766244140';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
