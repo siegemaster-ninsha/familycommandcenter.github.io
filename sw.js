a751fa22fe9c9d2f7da3dccc861b87e5275d3c2f
siegemaster-ninsha <ninsha@gmail.com>
2025-12-20 00:27:44 -0500

    Center calendar event dots vertically

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.78-1766208059';
+const CACHE_VERSION = 'v1.0.79-1766208462';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
