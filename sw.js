25357aaef65dc1df60b2d31b21d062b167eb9459
siegemaster-ninsha <ninsha@gmail.com>
2025-12-21 18:44:15 -0500

    Fix delete modal z-index to appear above flyout on mobile

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.91-1766360617';
+const CACHE_VERSION = 'v1.0.92-1766360653';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
