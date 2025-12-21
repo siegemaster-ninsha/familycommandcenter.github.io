bca31fdaae13e127a350291ecb7308f1dae2a22c
siegemaster-ninsha <ninsha@gmail.com>
2025-12-21 17:00:08 -0500

    Fix recipe scraping: add JSON-LD extraction, page type detection, improved error messages

--- config.js
+++ config.js
@@ -74,7 +74,7 @@ const CONFIG = {
   // Application Settings
   APP: {
     NAME: 'Family Command Center',
-    VERSION: '1.0.88 - Brilliant Tiger (Dec 21, 2025)',
+    VERSION: '1.0.89 - Ultimate Cheetah (Dec 21, 2025)',
     
     // Chore Categories (safe to be public)
     CATEGORIES: {
