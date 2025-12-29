79fbdb59f4db3dfb3ff39dc02cca8c76bbd0922c
siegemaster-ninsha <ninsha@gmail.com>
2025-12-29 17:48:49 -0500

    fix: constrain avatar chip buttons to 32x32px to match back arrow size

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
   // Application Settings
   APP: {
     NAME: 'Family Command Center',
-    VERSION: '1.0.114 - Xtreme Wolf (Dec 29, 2025)',
+    VERSION: '1.0.115 - Xtreme Urchin (Dec 29, 2025)',
     
     // Chore Categories (safe to be public)
     CATEGORIES: {
--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.114-1767047087';
+const CACHE_VERSION = 'v1.0.115-1767048527';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
