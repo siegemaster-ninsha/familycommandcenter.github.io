b8a5750e1e11ff9b4281f4b38ec9ec1c2f1d9618
siegemaster-ninsha <ninsha@gmail.com>
2025-12-29 15:00:46 -0500

    debug: add console logging and debounce to showReassignPicker for iOS Safari PWA diagnosis

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.105-1767038063';
+const CACHE_VERSION = 'v1.0.106-1767038444';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
