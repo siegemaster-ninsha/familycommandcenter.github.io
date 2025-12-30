3aa57278283468019b7358a85b3f447857fbaabf
siegemaster-ninsha <ninsha@gmail.com>
2025-12-29 19:47:43 -0500

    chore: test updated MCP deploy tool

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.116-1767051666';
+const CACHE_VERSION = 'v1.0.117-1767055663';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
