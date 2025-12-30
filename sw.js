e971a576697f7b65c362539fbe0f60320231f4e1
siegemaster-ninsha <ninsha@gmail.com>
2025-12-30 08:45:16 -0500

    Fix drag jitter - disable pointer events on child elements during drag-over

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.122-1767101967';
+const CACHE_VERSION = 'v1.0.123-1767102315';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
