3a56ca9c5025eb513aa9c1dd72347f8793e78ef9
siegemaster-ninsha <ninsha@gmail.com>
2025-12-30 08:39:27 -0500

    Fix drag-and-drop cursor feedback - hide browser's default Copy/X indicator

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
   // Application Settings
   APP: {
     NAME: 'Family Command Center',
-    VERSION: '1.0.121 - Wondrous Xerus (Dec 30, 2025)',
+    VERSION: '1.0.122 - Luminous Tiger (Dec 30, 2025)',
     
     // Chore Categories (safe to be public)
     CATEGORIES: {
--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.121-1767101519';
+const CACHE_VERSION = 'v1.0.122-1767101967';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
