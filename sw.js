f000f96ce04fbf894b01ba1d340daeced92e7828
siegemaster-ninsha <ninsha@gmail.com>
2026-01-02 17:29:36 -0500

    Fix showOnChoreBoard persistence - decode URL-encoded memberId in updateMember controller

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.175-1767391786';
+const CACHE_VERSION = 'v1.0.176-1767392976';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
