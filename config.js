f81fafbd9c7df53f79eeee9b8df4e77cb95338cf
siegemaster-ninsha <ninsha@gmail.com>
2026-01-11 18:39:35 -0500

    Fix shopping quick item creation - backend returns quickItem instead of item

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
   // Application Settings
   APP: {
     NAME: 'Family Command Center',
-    VERSION: '1.0.237 - Jubilant Panther (Jan 11, 2026)',
+    VERSION: '1.0.238 - Resilient Salamander (Jan 11, 2026)',
     
     // Chore Categories (safe to be public)
     CATEGORIES: {
--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 // IMPORTANT: This version MUST be updated with each deployment to bust the cache
 // The deploy script should update this automatically
-const CACHE_VERSION = 'v1.0.237-1768166507';
+const CACHE_VERSION = 'v1.0.238-1768174774';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
